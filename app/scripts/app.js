0edc0a0d7ccf1758e5e5b4682c1fcf82775e62f1
Christopher Diebold <cdiebold2012@gmail.com>
2016-11-03 09:34:23 -0400

    added logic to directive for drawing graph

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,16 +20,27 @@ var app = angular
   ]);
 
 // controllers
-app.controller('graphCtrl', function(){
+app.controller('graphCtrl', ['$log','$scope', 'mockDataService', function($log, $scope, mockDataService){
+  $log.info("Scope: " + $scope);
 
-});
+  $scope.data = mockDataService.mockData();
+  $scope.buttonTitle = mockDataService.title;
+  $log.info($scope.data.node); //I am able to get data from service
 
-app.controller('listCtrl', function(){
+}]);
 
-});
+app.controller('listCtrl', ['$scope','mockDataService', function($scope, mockDataService){
+  $scope.links = mockDataService.uniqueLinks;
+
+  //update links as they change
+  $scope.$watch('links', function(){
+    mockDataService.uniqueLinks = $scope.links;
+  });
+
+}]);
 
 // directives
-app.directive('directedGraph', ['$log','MockDataService', function($log, MockDataService){
+app.directive('directedGraph', ['$log','mockDataService', function($log, mockDataService){
   /**
    * defines the logic that manipulates the DOM
    * The link function is responsible for registering DOM listeners as well as updating the DOM. It is executed after
@@ -44,25 +55,93 @@ app.directive('directedGraph', ['$log','MockDataService', function($log, MockDat
     restrict: 'E', // can only use as an HTML attribute <directed-graph>
     templateUrl: '../views/directed-graph.view.html',
     scope: {
-      height: '=' // allows height to be defined as an attribute on the html
+      height: '=',  // allows height to be defined as an attribute on the html
+      nodes: '=',  //node data from controller
+      links: '='   // links data from controller
     },
     link: function(scope, element, attrs){
       var width = document.getElementById('root').clientWidth;
-      var svg = d3.select('svg').attr('height', attrs.height).attr('width', width);
       var height = svg.attr('height');
+
+      var svg = d3.select('svg').attr('height', height).attr('width', width);
+
       $log.info('svg height: ' + svg.attr('height'));
       $log.info('svg width: ' + svg.attr('width'));
 
       var color = d3.scaleOrdinal(d3.schemeCategory20);
-      $log.debug('assert color is not null: ' + color === undefined);
-      $log.info(color);
 
       var simulation = d3.forceSimulation()
-        .force("link", d3.forceLink().id(function(d) { return d.id; }))
-        .force("charge", d3.forceManyBody())
+        .force("link", d3.forceLink().distance(125).id(function(d) { return d.name; }))
+        .force("charge", d3.forceManyBody().strength(-75))
+        .force("collision", d3.forceCollide(5))
         .force("center", d3.forceCenter(width / 2, height / 2));
 
       $log.info(simulation);
+
+      //get data
+      var nodes = scope.nodes;
+      var edges = scope.links;
+
+
+
+      function initializeGraph(svg, color, simulation){
+        var link = svg.append('g')
+          .attr('class', 'links')
+          .selectAll('line')
+          .data(edges)
+          .enter().append('line')
+          .attr('stroke', 'black')
+          .attr('stroke-width', 1.5);
+
+        var node = svg.selectAll('nodes')
+          .attr('class', 'nodes')
+          .data(nodes)
+          .enter().append('g')
+          .append('circle')
+          .attr('r', 20)
+          .attr('fill', function(d){ return color(d.group)})
+          .call(d3.drag().on('start', dragstarted).on('drag', dragged).on('end', dragended));
+
+
+
+        dragstarted = function(d){
+          if(!d3.event.active){
+            simulation.alphaTarget(0.3).restart();
+          }
+          d.fx = d.x;
+          d.fy = d.y;
+
+        };
+        dragged = function(d){
+          d.fx = d3.event.x;
+          d.fy = d3.event.y;
+        };
+        dragended = function(d){
+          if(!d3.event.active){
+            simulation.alphaTarget(0);
+          }
+          d.fx = null;
+          d.fy = null;
+        };
+
+        ticked = function(){
+          link.attr('x1', function(d){ return d.source.x; })
+            .attr('y1'  , function(d){ return d.source.y; })
+            .attr('x2'  , function(d){ return d.target.x; })
+            .attr('y2'  , function(d){ return d.target.y; });
+
+          node.attr('cx', function(d){ return d.x; })
+            .attr('cy'  , function(d){ return d.y; });
+        };
+
+        simulation.nodes(node).on('tick', ticked);
+        simulation.force('link').links(link);
+
+      }
+
+      initializeGraph(svg, color, simulation);
+
+
 
 
 
--- app/scripts/services/mockdata-service.js
+++ app/scripts/services/mockdata-service.js
@@ -8,31 +8,33 @@
  * Service in the directedGraphApp.
  */
 angular.module('directedGraphApp')
-  .service('MockDataService', function () {
+  .service('mockDataService', function () {
+    this.mockData = function(){
+      var data = {};
+
+      data.node = [
+        { id:'1234', name:'node1', group:1},
+        { id:'5678', name:'node2', group:2},
+        { id:'3210', name:'node3', group:3},
+        { id:'0984', name:'node4', group:4}
+      ];
+
+      data.edge = [
+        {source: 'node1', target: 'node2', name: 'type1'},
+        {source: 'node1', target: 'node3', name: 'type2'},
+        {source: 'node2', target: 'node2', name: 'type1'},
+        {source: 'node3', target: 'node4', name: 'type4'}
+      ];
+
+      return data;
+    };
+
+    this.title = 'myTitle';
+    this.uniqueLinks = ['type1', 'type2', 'type4'];
+
     // AngularJS will instantiate a singleton by calling "new" on this function
-    var data = createMockData();
+    //var data = createMockData();
 
   });
 
-  function createMockData(){
-    var data = {};
 
-    data.node = [
-      { id:'1234', name:'node1', group:1},
-      { id:'5678', name:'node2', group:2},
-      { id:'3210', name:'node3', group:3},
-      { id:'0984', name:'node4', group:4}
-    ];
-
-    data.edge = [
-      {source: 'node1', target: 'node2', name: 'type1'},
-      {source: 'node1', target: 'node3', name: 'type2'},
-      {source: 'node2', target: 'node2', name: 'type1'},
-      {source: 'node3', target: 'node4', name: 'type4'}
-    ];
-
-   data.title = 'myName';
-
-    return data;
-
-  }
