b9382fbc092492c5ab045920142720a9be54a3bb
cdiebold <cdiebold2012@gmail.com>
2016-11-03 12:41:02 -0400

    got initial graph to draw

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,15 +9,7 @@
  * Main module of the application.
  */
 var app = angular
-  .module('directedGraphApp', [
-    'ngAnimate',
-    'ngAria',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ]);
+  .module('directedGraphApp', ['ngAnimate']);
 
 // controllers
 app.controller('graphCtrl', ['$log','$scope', 'mockDataService', function($log, $scope, mockDataService){
@@ -40,7 +32,7 @@ app.controller('listCtrl', ['$scope','mockDataService', function($scope, mockDat
 }]);
 
 // directives
-app.directive('directedGraph', ['$log','mockDataService', function($log, mockDataService){
+app.directive('directedGraph', ['$log','mockDataService', function($log, mockDataService) {
   /**
    * defines the logic that manipulates the DOM
    * The link function is responsible for registering DOM listeners as well as updating the DOM. It is executed after
@@ -51,103 +43,102 @@ app.directive('directedGraph', ['$log','mockDataService', function($log, mockDat
    * already been linked.
    * @param attrs - is a hash object with key-value pairs of normalized attribute names and their corresponding attribute values
    */
-  return{
+
+  function link(scope, element, attributes){
+    var width = document.getElementById('root').clientWidth;
+    var svg = d3.select('svg').attr('height', attributes.height).attr('width', width);
+    var height = svg.attr('height');
+    $log.info('svg height: ' + svg.attr('height'));
+    $log.info('svg width: ' + svg.attr('width'));
+
+    var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+    var simulation = d3.forceSimulation()
+      .force("link", d3.forceLink().distance(125).id(function (d) {
+        return d.name;
+      }))
+      .force("charge", d3.forceManyBody().strength(-75))
+      .force("collision", d3.forceCollide(5))
+      .force("center", d3.forceCenter(width / 2, height / 2));
+
+    var data = mockDataService.mockData();
+
+    //get data
+    var graph_nodes = data.node;
+    var graph_edges = data.edge;
+
+    $log.info("nodes data undefined: " + graph_nodes);
+    $log.info("links data undefined: " + graph_edges);
+
+    var link = svg.append('g')
+      .attr('class', 'links')
+      .selectAll('line')
+      .data(graph_edges)
+      .enter().append('line')
+      .attr('stroke', 'black')
+      .attr('stroke-width', 1.5);
+
+    $log.info("links inside init: " + link);
+
+    var node = svg.append("g")
+      .attr("class", "nodes")
+      .selectAll("circle")
+      .data(graph_nodes)
+      .enter().append("circle")
+      .attr("r", 20)
+      .attr("fill", function (d) {
+        return color(d.group);
+      })
+      .call(d3.drag()
+        .on("start", dragStarted)
+        .on("drag", dragged)
+        .on("end", dragEnded));
+
+    $log.info("node inside init: " + node);
+    var ticked = function () {
+      link.attr('x1', function (d) { return d.source.x; })
+        .attr('y1'  , function (d) { return d.source.y; })
+        .attr('x2'  , function (d) { return d.target.x; })
+        .attr('y2'  , function (d) { return d.target.y; });
+
+      node.attr('cx', function (d) { return d.x; }).attr('cy', function (d) { return d.y; });
+
+    }; //end ticked function
+
+    simulation.nodes(graph_nodes).on('tick', ticked);
+    simulation.force('link').links(graph_edges);
+
+    function dragStarted(d) {
+      if (!d3.event.active) {
+        simulation.alphaTarget(0.3).restart();
+      }
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+
+    function dragged(d) {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+    }
+
+    function dragEnded(d) {
+      if (!d3.event.active) {
+        simulation.alphaTarget(0);
+      }
+      d.fx = null;
+      d.fy = null;
+    }
+
+  }
+  return {
     restrict: 'E', // can only use as an HTML attribute <directed-graph>
     templateUrl: '../views/directed-graph.view.html',
     scope: {
-      height: '=',  // allows height to be defined as an attribute on the html
-      nodes: '=',  //node data from controller
-      links: '='   // links data from controller
+      height: '=' // allows height to be defined as an attribute on the html
     },
-    link: function(scope, element, attrs){
-      var width = document.getElementById('root').clientWidth;
-      var height = svg.attr('height');
+    link: link
+  }; // end return object
 
-      var svg = d3.select('svg').attr('height', height).attr('width', width);
-
-      $log.info('svg height: ' + svg.attr('height'));
-      $log.info('svg width: ' + svg.attr('width'));
-
-      var color = d3.scaleOrdinal(d3.schemeCategory20);
-
-      var simulation = d3.forceSimulation()
-        .force("link", d3.forceLink().distance(125).id(function(d) { return d.name; }))
-        .force("charge", d3.forceManyBody().strength(-75))
-        .force("collision", d3.forceCollide(5))
-        .force("center", d3.forceCenter(width / 2, height / 2));
-
-      $log.info(simulation);
-
-      //get data
-      var nodes = scope.nodes;
-      var edges = scope.links;
-
-
-
-      function initializeGraph(svg, color, simulation){
-        var link = svg.append('g')
-          .attr('class', 'links')
-          .selectAll('line')
-          .data(edges)
-          .enter().append('line')
-          .attr('stroke', 'black')
-          .attr('stroke-width', 1.5);
-
-        var node = svg.selectAll('nodes')
-          .attr('class', 'nodes')
-          .data(nodes)
-          .enter().append('g')
-          .append('circle')
-          .attr('r', 20)
-          .attr('fill', function(d){ return color(d.group)})
-          .call(d3.drag().on('start', dragstarted).on('drag', dragged).on('end', dragended));
-
-
-
-        dragstarted = function(d){
-          if(!d3.event.active){
-            simulation.alphaTarget(0.3).restart();
-          }
-          d.fx = d.x;
-          d.fy = d.y;
-
-        };
-        dragged = function(d){
-          d.fx = d3.event.x;
-          d.fy = d3.event.y;
-        };
-        dragended = function(d){
-          if(!d3.event.active){
-            simulation.alphaTarget(0);
-          }
-          d.fx = null;
-          d.fy = null;
-        };
-
-        ticked = function(){
-          link.attr('x1', function(d){ return d.source.x; })
-            .attr('y1'  , function(d){ return d.source.y; })
-            .attr('x2'  , function(d){ return d.target.x; })
-            .attr('y2'  , function(d){ return d.target.y; });
-
-          node.attr('cx', function(d){ return d.x; })
-            .attr('cy'  , function(d){ return d.y; });
-        };
-
-        simulation.nodes(node).on('tick', ticked);
-        simulation.force('link').links(link);
-
-      }
-
-      initializeGraph(svg, color, simulation);
-
-
-
-
-
-    }
-
-  };
 }]);
 
 app.directive('listView', function(){
